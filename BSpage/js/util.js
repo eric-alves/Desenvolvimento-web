dcd8b679af644a400c9a23ccba5e6cd59bce37b5
Eric dos Reis Alves <eralves01@hotmail.com>
2020-01-20 17:17:52 -0300

    Teste para BS
    Este projeto contem apenas as telas HTML para um site. Estão contidos nele as telas de login, cadastro e listagem dos cadastrados.

---
+++ BSpage/js/util.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#cpf').mask('000.000.000-00');
+    $('#tel').mask('(00) 0000-0000');
+    $('#cel').mask('(00) 00000-0000');
+    $('#cep').mask('00000-000');
+});
+
+function alertCadastro(){
+    alert("Salvo com sucesso!");
+};
